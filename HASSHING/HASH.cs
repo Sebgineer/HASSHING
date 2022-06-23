b9acd7bcee30b802df2df533e6e125d318f39132
Sebgineer <sebastian-karlsen@hotmail.com>
2022-06-23 09:32:11 +0200

    Add project files.

---
+++ HASSHING/HASH.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace HASSHING
+{
+    internal class HASH
+    {
+        private HMAC hMAC;
+
+        public void setHmac(string name){
+            switch (name)
+            {
+                case "MD5":
+                    this.hMAC = new HMACMD5();
+                    break;
+                case "SHA1":
+                    this.hMAC = new HMACSHA1();
+                    break;
+                case "SHA256":
+                    this.hMAC = new HMACSHA256();
+                    break;
+            }
+
+        }
+
+        public byte[] randomKey()
+        {
+            RNGCryptoServiceProvider rnd = new RNGCryptoServiceProvider();
+            byte[] key = new byte[4];
+            rnd.GetBytes(key);
+            return key;
+
+        }
+
+        public byte[] Hashing(byte[] message, byte[] key)
+        {
+            hMAC.Key = key;
+            return hMAC.ComputeHash(message);
+        }
+    }
+}
---
+++ HASSHING/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HASSHING
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HASH hash = new HASH();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            hash.setHmac("MD5");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] message = Encoding.ASCII.GetBytes(Message.Text);
+            byte[] key = Encoding.ASCII.GetBytes(Key.Text);
+            byte[] value = hash.Hashing(message, key);
+            HashedText.Content = Convert.ToBase64String(value);
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem item = (ComboBoxItem)Select.SelectedItem;
+            if (item.Content != null)
+            {
+                hash.setHmac(item.Content.ToString());
+            }
+        }
+    }
+}
